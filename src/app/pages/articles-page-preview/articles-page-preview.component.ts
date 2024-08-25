4ed867db5b47bb7089b59e256bc8f5abd6117394
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2024-08-25 23:20:03 +0300

    edit render html returned from api

--- src/app/pages/articles-page-preview/articles-page-preview.component.ts
+++ src/app/pages/articles-page-preview/articles-page-preview.component.ts
@@ -1,56 +1,60 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { DataService } from 'src/app/data.service';
-DataService
 
 @Component({
   selector: 'app-articles-page-preview',
   templateUrl: './articles-page-preview.component.html',
-  styleUrls: ['./articles-page-preview.component.css']
+  styleUrls: ['./articles-page-preview.component.css'],
 })
 export class ArticlesPagePreviewComponent implements OnInit {
+  id: string = '';
+  articles: any = {
+    description: '', // Add description here to prevent undefined errors
+  };
+  moreArticles: any = {};
+  days!: number;
 
-  id :string = "";
-  articles :any = {};
-  moreArticles:any ={}
-  days!:number
-  constructor(private _ActivatedRoute:ActivatedRoute,private _DataService:DataService) {
+  constructor(
+    private _ActivatedRoute: ActivatedRoute,
+    private _DataService: DataService,
+    private sanitizer: DomSanitizer // Inject DomSanitizer
+  ) {}
 
-
-  }
   ngOnInit(): void {
-    this.id = this._ActivatedRoute.snapshot.params['id']
-    this._DataService.getNewsDetials(this.id).subscribe((response)=> {
+    this.id = this._ActivatedRoute.snapshot.params['id'];
+    this._DataService.getNewsDetials(this.id).subscribe((response) => {
       this.articles = response.data;
 
+      // Sanitize the description HTML
+      this.articles.description = this.sanitizeHtml(this.articles.description);
 
-      this.moreArticles = response["More Articles"]
+      this.moreArticles = response['More Articles'];
+
       let time = new Date(this.articles.date);
-      console.log(this.articles.date);
-
-console.log(this.moreArticles);
 
       let dateString = time;
 
-  // Convert the date string to a Date object
-  var dateToSubtract = new Date(dateString);
+      // Convert the date string to a Date object
+      var dateToSubtract = new Date(dateString);
 
-  // Get the current date
-  var currentDate = new Date();
+      // Get the current date
+      var currentDate = new Date();
 
-  // Calculate the difference in milliseconds
-  var difference = currentDate.getTime() - dateToSubtract.getTime();
+      // Calculate the difference in milliseconds
+      var difference = currentDate.getTime() - dateToSubtract.getTime();
 
-  // Convert milliseconds difference to days
-  var millisecondsPerDay = 1000 * 60 * 60 * 24;
-  var differenceInDays = Math.floor(difference / millisecondsPerDay);
+      // Convert milliseconds difference to days
+      var millisecondsPerDay = 1000 * 60 * 60 * 24;
+      var differenceInDays = Math.floor(difference / millisecondsPerDay);
 
-
-  this.days = differenceInDays;
-
-    })
-
+      this.days = differenceInDays;
+    });
   }
 
-
+  // Method to sanitize HTML
+  sanitizeHtml(html: string): SafeHtml {
+    return this.sanitizer.bypassSecurityTrustHtml(html);
+  }
 }
